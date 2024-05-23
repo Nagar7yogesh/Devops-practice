8e9ada7d3e4055d1d44dbf85f8388b581177ac2b
Yogesh Nagar <yogeshnagar@Yogeshs-MacBook-Air.local>
2024-05-23 15:29:17 +0530

    checking readme file

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  credentials = file("dealofday-398415-7cc59cb795d1.json")
+  project     = "dealofday-398415"
+  region      = "asia-south1"
+}
+
+resource "google_compute_instance" "my_instance" {
+  name         = "my-vm-instance"
+  machine_type = "e2-micro"
+  zone         = "asia-south1-c"
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
