5886e1a9e057d969213312d7af2d5a4134c3fbf8
Yogesh Nagar <yogeshnagar@Yogeshs-MacBook-Air.local>
2024-05-24 20:02:26 +0530

    Create VM

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,19 +3,3 @@ provider "google" {
   region      = "us-central1"
 }
 
-resource "google_compute_instance" "my_instance" {
-  name         = "my-vm-instance"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-10"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {}
-  }
-}
